521687f0ea2c7690d8d22bac3d348a4aea655020
Davide Gessa <gessadavide@gmail.com>
2015-12-07 00:07:08 +0100

    jsonCall and samples

--- cvm.js
+++ cvm.js
@@ -1,12 +1,19 @@
-$.jsonRPC.setup({
-  endPoint: 'http://localhost:8181/',
-  namespace: ''
-});
 
-
-var rpc = function (method, params, cb) {
+var cvm_RPCCall = function (host, method, params, cb) {
 	$.jsonRPC.request(method, {
+		endPoint: 'http://' + host,
+		namespace: '',
   		params: params, 
 		success: function (data) { cb (null, data['result']); }, 
 		error: function (data) { cb (data['error'], null); } });
 };
+
+var cvm_jsonLocalCall = function (method, params, cb) {
+	return cvm_RPCCall ('localhost:8181', method, params, cb);
+};
+
+
+var cvm = {
+	jsonLocalCall: cvm_jsonLocalCall
+};
+
